c309d58fd21589e8daeaacd11e099124b5dcaa3a
kajacx <kajacx@gmail.com>
2025-08-23 21:23:11 +0200

    Implement a line numbers counter

--- src/grammar/spanned.rs
+++ src/grammar/spanned.rs
@@ -27,3 +27,53 @@ impl<T> Spanned<T> {
 pub trait SpanIterator {
     fn list_spans(&mut self, visitor: impl FnMut(&mut Span));
 }
+
+pub struct LineNumbers {
+    /** Stores where the first character on each line is, 0-indexed. */
+    pub newline_positions: Vec<usize>,
+}
+
+impl LineNumbers {
+    pub fn new(content: &str) -> Self {
+        let mut positions = vec![0];
+
+        for (i, ch) in content.char_indices() {
+            if ch == '\n' {
+                positions.push(i + 1);
+            }
+        }
+
+        Self {
+            newline_positions: positions,
+        }
+    }
+
+    /**
+     * Offset is 0-indexed, line and column are both 1-indexed
+     */
+    pub fn get_line_and_column(&self, offset: usize) -> (usize, usize) {
+        // Everything inside is zero indexed
+        let line = self
+            .newline_positions
+            .iter()
+            .filter(|pos| **pos <= offset)
+            .count()
+            - 1;
+
+        let column = offset - self.newline_positions[line];
+
+        (line + 1, column + 1)
+    }
+}
+
+#[test]
+fn test_line_numbers() {
+    let line_numbers = LineNumbers::new("Hello\nworld\n\nHowdy!");
+    assert_eq!(line_numbers.get_line_and_column(0), (1, 1));
+    assert_eq!(line_numbers.get_line_and_column(4), (1, 5));
+    assert_eq!(line_numbers.get_line_and_column(6), (2, 1));
+    assert_eq!(line_numbers.get_line_and_column(10), (2, 5));
+    assert_eq!(line_numbers.get_line_and_column(13), (4, 1));
+    assert_eq!(line_numbers.get_line_and_column(18), (4, 6));
+    assert_eq!(line_numbers.get_line_and_column(20), (4, 8)); // predictable behavior after eof
+}
