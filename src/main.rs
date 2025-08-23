3c2e28d3f45c991bfe38acc225be4c551b34b3d7
kajacx <kajacx@gmail.com>
2025-08-23 11:52:33 +0200

    Test syntax works

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,32 @@ use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(luasyn);
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum LuaStatement {
+    Comment(LuaComment),
+    VarDeclaration { name: String, value: LuaExpression },
+}
+
+// #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+// pub struct LuaComment(Vec<LuaCommentToken>);
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum LuaComment {
+    TypeAnnotation(LustType),
+    Text(String),
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum LuaExpression {
+    StringLiteral(String),
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum LustType {
+    Number,
+    String,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
 enum TestResult {
     Pass,
     Warn,
@@ -10,6 +36,8 @@ enum TestResult {
 }
 
 fn main() {
+    test_it();
+
     let path = std::env::args().nth(1).expect("no path given");
 
     let expected = if path.ends_with("_pass.lua") {
@@ -32,13 +60,13 @@ fn main() {
 }
 
 fn analyze_file(filename: &str) -> TestResult {
-    let content = std::fs::read_to_string(filename).expect("Read file content");
+    let content = std::fs::read_to_string(filename).unwrap();
     analyze_file_content(&content)
 }
 
 fn analyze_file_content(content: &str) -> TestResult {
-    let parser = luasyn::TermParser::new();
-    let parsed = match parser.parse(content) {
+    let parser = luasyn::LuaStatementsParser::new();
+    let statements = match parser.parse(content) {
         Ok(value) => value,
         Err(err) => {
             eprintln!("Error when parsing the input file:");
@@ -47,5 +75,31 @@ fn analyze_file_content(content: &str) -> TestResult {
         }
     };
 
+    println!("{statements:?}");
     TestResult::Pass
+}
+
+lalrpop_mod!(testsyn);
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum TestStatement {
+    Comment(TestComment),
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum TestComment {
+    TypeAnnotation(TestType),
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum TestType {
+    Number,
+    String,
+}
+
+fn test_it() {
+    let content = std::fs::read_to_string("tests/testsyn.lua").unwrap();
+    let parser = testsyn::TestStatementParser::new();
+    let statement = parser.parse(&content).unwrap();
+    println!("{statement:?}");
 }
